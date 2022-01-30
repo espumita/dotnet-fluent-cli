303e0b7d7ebd96e3bbabbea014bd0b01984b4ba1
espumita <davidfalconnaranjo@gmail.com>
2022-01-30 15:59:39 +0000

    Green: do_not_read_any_flag

---
+++ src/Dotnet.Cli.Args/CliArgsBuilder.cs
@@ -0,0 +1,28 @@
+﻿namespace Dotnet.Cli.Args; 
+
+public class CliArgsBuilder {
+    private readonly string[] arg;
+
+    private CliArgsBuilder(string[] arg) {
+        this.arg = arg;
+    }
+
+    public static CliArgsBuilder From(string[] args) {
+        return new CliArgsBuilder(args);
+    }
+
+    public ArgsOptions Build() {
+        return new ArgsOptions();
+    }
+}
+
+public class ArgsOptions {
+    public ArgsOptions() {
+        Flags = new List<FlagOption>();
+    }
+
+    public List<FlagOption> Flags { get; set; }
+}
+
+public class FlagOption {
+}
---
+++ tests/Dotnet.Cli.Args.Tests/CliArgsBuilderTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Dotnet.Cli.Args.Tests; 
+
+public class CliArgsBuilderTests {
+
+    private StringBuilder stringBuilder;
+
+    [SetUp]
+    public void SetUp() {
+        stringBuilder = new StringBuilder();
+    }
+
+
+    [Test]
+    public void do_not_read_any_flag() {
+        var environmentArgs = new string[] { };
+
+        var args = CliArgsBuilderFrom(environmentArgs)
+            .Build();
+
+        args.Flags.Should().BeEmpty();
+    }
+
+    private static CliArgsBuilder CliArgsBuilderFrom(string[] args) {
+        return Args.CliArgsBuilder.From(args);
+    }
+}
